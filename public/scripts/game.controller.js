9af0039ff3669b529afbe1c1ddfbb1897293b5ea
Silmi@L <89022084+leandrMachado@users.noreply.github.com>
2023-05-29 16:24:47 +0100

    Change command line style and change system to read comands

--- public/scripts/game.controller.js
+++ public/scripts/game.controller.js
@@ -1,88 +1,16 @@
-let command = '';
-let inventory = ['mapa'];
+let command = "";
+let inventory = ["mapa"];
 let chapterIndex = 2; // init 0
 let historyIndex = 0;
 
 const commands = {
-    "move": () => readChapter(),
-    "look": () => {},
-    "examine": () => {},
-    "inventory": () => inventoryLoad(),
-    "help": async () => {
-        await typing("Commands: " + JSON.stringify(Object.keys(commands)).replace(/["']/g, ''))
-    }
-}
-
-let chapter = 0;
-let indexLine = 0;
-
-const readChapter = async () => {
-    const history = await historyRead();
-    console.log(chapter)
-
-    for (let index = indexLine; index < history.length; index++) {
-
-        if (history[index] === `Capítulo - ${chapter+1}`) {
-            chapter++;
-            indexLine++
-            await typing(history[index], true);
-        }
-        else if (history[index] === "move()") {
-
-            console.log('ananas move')
-            break;
-        }
-        else if (history[index] === "look()") {
-
-            console.log('ananas look')
-            break;
-        }
-        else if (history[index] === "examine()"){
-
-            console.log('ananas examine')
-            break;
-        }
-        else 
-        await typing(history[index]);
-            indexLine++
-    }
-}
-
-new KeyPressListener(async (key) => {
-    switch (key) {
-        case 8: {
-            command = command.slice(0, -1);
-            document.getElementById('bash').innerHTML = command;
-            break;
-        }
-        case 32: {
-            command += ' ';
-            document.getElementById('bash').innerHTML = command;
-            break;
-        }
-        case 13: {
-            if (command in commands) {
-                commands[command]();
-                command = '';
-                document.getElementById('bash').innerHTML = command;
-            }
-            else {
-                command = '';
-                document.getElementById('bash').innerHTML = command;
-                console.log('Comando nao encontrado escrever help para ter mais!')
-            }
-            
-            break;
-        }
-        default: {
-            command += String.fromCharCode(key).toLowerCase();
-            document.getElementById('bash').innerHTML = command;
-            break;
-        }
-    }
-})
-
-if (window.innerWidth < 750) 
-    typing('This website is not compatible with mobile devices...');
-else
-    typing('Digite (move) para iniciar ou (help) a mostrar comandos');
+  move: () => readChapter(),
+  look: () => {},
+  examine: () => {},
+  inventory: () => inventoryLoad(),
+  help: async () => {
+    await typing(
+      "Commands: " + JSON.stringify(Object.keys(commands)).replace(/["']/g, "")
+    );
+  },
+};
